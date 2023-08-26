4cb451870951af9526c8f754159511fb753dc443
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-08-27 02:25:28 +0900

    新規登録/本人情報確認のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
    
   validates :email, presence: true, format: {with: /@/ }
   validates :password, presence: true, length: {minimum: 6}, format: {with:  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :kane_last_name, :kana_first_name, presence: true, format: {with:  /\A[a-z0-9]+\z/i}
 end
