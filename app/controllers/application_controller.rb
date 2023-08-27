52d47014da3c60572386793e78d43799f2c8b693
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-08-28 00:50:47 +0900

    新規登録ができること

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  
   
 private
 
@@ -7,5 +10,10 @@ private
   authenticate_or_request_with_http_basic do |username, password|
     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
    end
+ end
+
+ private
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kane_last_name, :kana_first_name, :birth])
  end
 end    
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-   
+         :recoverable, :rememberable, :validatable 
+         
+  validates :nickname, presence: true
   validates :email, presence: true, format: {with: /@/ }
   validates :password, presence: true, length: {minimum: 6}, format: {with:  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :kane_last_name, :kana_first_name, presence: true, format: {with:  /\A[a-z0-9]+\z/i}
+  validates :kane_last_name, :kana_first_name, presence: true, format: {with: /\A[ァ-ヶー]+\z/ }
 end
