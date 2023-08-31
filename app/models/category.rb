bbf37a854e57a0a74ea292a9e5ecac4242fc3e2d
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-09-01 00:37:42 +0900

    出品入力欄の実装

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ActiveHash::Base
+  self.data = [
+    {id: 0, name: '---'},{id: 1, name: 'メンズ'}, {id: 2, name:'レディース'}, {id: 3, name: 'ベビー・キッズ'}, {id: 4, name: 'インテリア'}, {id: 5, name: '住まい・小物'},
+    {id: 6, name: '本・音楽・ゲーム'}, {id: 7, name: 'おもちゃ・ボビー・グッズ'}, {id: 8, name: '家電・スマホ・カメラ'}, {id: 9, name: 'レジャー'}, {id: 10, name: 'ハンドメイド'}, {id: 11, name: 'その他'}
+   ]
+end   
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
+
   has_one_attached :image
 
-  validates :item_name, :description, :price, presence: true
+  validates :item_name, :description, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id,:shipping_day_id,  presence: true
+  validates :price,  presence: true,
+                     numerisality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
+                                   format: {:with: /\A[a-z]+\z/ }
 end
