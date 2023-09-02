ddc4ebad3bc18313fcdcca7d39efd0605c62c345
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-09-03 01:53:09 +0900

    itemモデル単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,18 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
-
-  validates :item_name, :description, presence: true
-  validates :category_id, numericality: { other_than: 0 }, presence: true
-  validates :condition_id,numericality: { other_than: 0 }, presence: true
-  validates :shipping_fee_id, numericality: { other_than: 0 }, presence: true
-  validates :shipping_area_id, numericality: { other_than: 0 }, presence: true
-  validates :shipping_day_id, numericality: { other_than: 0 }, presence: true
+  
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 0 }
+  validates :condition_id,numericality: { other_than: 0 }
+  validates :shipping_fee_id, numericality: { other_than: 0 }
+  validates :shipping_area_id, numericality: { other_than: 0 }
+  validates :shipping_day_id, numericality: { other_than: 0 }
   validates :price,  presence: true,
                      numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999},
-                     format: {with: /\A[0-9]+\z/}
+                     format: {with: /\A\d+\z/}
 
                                 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {'りんご' }
+    description {'りんごあめ'}
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_fee_id { 2 }
+    shipping_area_id { 2 }
+    shipping_day_id { 2 }
+    price { Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'),filename: 'test_image.png')
+    end  
   end
 end
