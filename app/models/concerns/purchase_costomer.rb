ff0983fd62f0f4c2ec86b9d243dba81fb67ef94b
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-09-09 15:27:43 +0900

    発送先情報のバリデーション設定

---
+++ app/models/concerns/purchase_costomer.rb
@@ -0,0 +1,12 @@
+class PurchaseCostomer
+  include ActiveModel::ActiveModel
+  attr_accessor :postal_code, :shipping_area_id, :city, :address,
+                :building,:phone_num, :user_id, :item_id
+  
+  
+    validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/}
+    validates :shipping_area_id, numericality: { other_than: 0 }
+    validates :city, presence: true
+    validatas :address, presence: true
+    validates :phone_num, format: {with: /\A\d{10,11}\z/}
+  end  
--- db/migrate/20230828210615_create_costomers.rb
+++ db/migrate/20230828210615_create_costomers.rb
@@ -4,7 +4,7 @@ class CreateCostomers < ActiveRecord::Migration[6.0]
       t.string, :postal_code,        null: false 
       t.integer, :shipping_area_id,  null: false
       t.string, :address,            null: false
-      t.string, :building,
+      t.string, :building
      |t.string, :phone_num,          null: false
       t.references, :purchase,       null: false, foreign_key:true 
       t.timestamps
