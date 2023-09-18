57d59afb304bb093e811531dcaeecb9627df6de0
chitachikuwa <136873298+chitachikuwa@users.noreply.github.com>
2023-09-18 17:41:37 +0900

    訂正

--- db/migrate/20230828210615_create_costomers.rb
+++ db/migrate/20230828210615_create_costomers.rb
@@ -7,7 +7,7 @@ class CreateCostomers < ActiveRecord::Migration[6.0]
       t.string :address,            null: false
       t.string :building
       t.string :phone_num,          null: false
-      t.references :purchase,       null: false, foreign_key: true 
+      t.references :purchase,       null: false, foreign_key: true
       t.timestamps
     end
   end
